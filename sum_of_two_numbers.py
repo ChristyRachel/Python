e02195ece61ad1f3f65d888c7f25ae93445b13f2
Christy Rachel Philip <40349694+ChristyRachel@users.noreply.github.com>
2020-08-23 22:56:59 -0400

    Add files via upload

---
+++ sum_of_two_numbers.py
@@ -0,0 +1,10 @@
+
+# Python program to add two numbers 
+ 
+number_1 = 15
+number_2 = 12
+  
+sum = number_1 + number_2 
+  
+# to print the out
+print("{0} + {1} = {2}" .format(number_1, number_2, sum)) 
