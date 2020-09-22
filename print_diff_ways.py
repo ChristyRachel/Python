deeaa87220ecd8310953e246ae9e5e35582f39bf
Christy Rachel Philip <40349694+ChristyRachel@users.noreply.github.com>
2020-09-22 13:34:29 -0400

    Add files via upload
    This one shows different ways to print

---
+++ print_diff_ways.py
@@ -0,0 +1,15 @@
+colors = ['red','blue']
+print (colors)
+
+a = (145, 678, 'Christy Rachel Philip', 456)
+print (a)
+
+a = 5; b=6; c=7;
+print (a,b,c)
+
+print ("This is a way to print directly")
+
+def function_name():
+    "This is a way to print from function by calling function"
+    return 
+print(function_name.__doc__)
