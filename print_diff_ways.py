3646bf284b96dab8bcff6cc8975c0b677e6ab822
Christy Rachel Philip <40349694+ChristyRachel@users.noreply.github.com>
2020-09-23 13:58:08 -0400

    Update print_diff_ways.py

--- print_diff_ways.py
+++ print_diff_ways.py
@@ -13,3 +13,30 @@ def function_name():
     "This is a way to print from function by calling function"
     return 
 print(function_name.__doc__)
+
+strings = "This is Python"
+char = "C"
+multiline_string = """This a 
+multiline string 
+with 
+more than one line code."""
+unicode = u"\u00dcnic\u00f6de"
+raw_str = r"raw \n string"
+
+print(strings)
+print(char)
+print(multiline_string)
+print(unicode)
+print(raw_str)
+
+x = 3 + 3.14j
+print(x, x.imag, x.real)
+
+binary = 0b101
+print (binary)
+
+octal = 0o101
+print (octal)
+
+hexadecimal = 0x101
+print (hexadecimal)
