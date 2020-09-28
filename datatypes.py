1999c23fe883084e9c017a9b6aa9e1171ee9fe18
Christy Rachel Philip <40349694+ChristyRachel@users.noreply.github.com>
2020-09-27 23:59:49 -0400

    Add files via upload
    This includes different data types such as float, int, set, list, string, dict and set

---
+++ datatypes.py
@@ -0,0 +1,30 @@
+a = 5
+print (type(a))
+
+b = 2.3
+print (type(b))
+
+c = {1, 2.3, 12}
+print (type(c))
+
+d = "abcd"
+print (type(d))
+
+e = 'a'
+print (type(e))
+
+f = '''multiline
+string'''
+print (type(f))
+
+g = {25: 'team','member':12}
+print (type(g))
+
+h = ['l','i','s','t']
+print (type(h))
+
+i = {3,4,5}
+print (type(i))
+
+j = {34:1, 23:67}
+print (type(j))
