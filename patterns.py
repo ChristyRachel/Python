2ef38253693a938f7d50fb2ebf690a213b1af0d8
Christy Rachel Philip <40349694+ChristyRachel@users.noreply.github.com>
2020-09-30 02:38:37 -0400

    Add files via upload
    Print the following pattern
    
    1 
    2 2 
    3 3 3 
    4 4 4 4 
    5 5 5 5 5

---
+++ patterns.py
@@ -0,0 +1,18 @@
+'''
+
+Print the following pattern
+
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5
+
+'''
+for number in range (1,6):
+    for i in range (number):
+        print (number)
+print ("\n")
+    
+
+
