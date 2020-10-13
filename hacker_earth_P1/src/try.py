97b2d4d9af4ca2440a0a9d0de2fadd23f6de3261
Christy Rachel Philip <40349694+ChristyRachel@users.noreply.github.com>
2020-10-13 03:21:02 -0400

    Update try.py

--- hacker_earth_P1/src/try.py
+++ hacker_earth_P1/src/try.py
@@ -4,8 +4,7 @@ Created on Oct. 12, 2020
 @author: christy
 '''
 
-chr_1 = '70'
-chr_2 = '81'
-for i in range(int(chr_1),int(chr_2)):
+
+for i in range(ord('F'),ord('Q')):
     print(i)
-    
+        
